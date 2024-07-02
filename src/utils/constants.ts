a875497acc45dedd9a460212ce27f48a7a2c0572
Sanjay Bhaambhu <bhaambhu@gmail.com>
2024-07-02 21:24:36 +0530

    test

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,5 @@
 const API_URL_LIVE = "https://scalevote.up.railway.app/api";
 const API_URL_LOCAL = "http://localhost:8080/api";
 export const BASE_URL = import.meta.env.BASE_URL;
-export const API_URL = import.meta.env.DEV ? API_URL_LOCAL : API_URL_LIVE;
+// export const API_URL = import.meta.env.DEV ? API_URL_LOCAL : API_URL_LIVE;
+export const API_URL = API_URL_LIVE;
